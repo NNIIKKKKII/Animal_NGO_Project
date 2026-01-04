98393ce48bd1a330b3fc229e31eeeaaf2599f456
Nikki <nikhilpareeshwad@gmail.com>
2026-01-04 15:05:12 +0530

    fix index.js RENDER

--- animal-ngo-backend/index.js
+++ animal-ngo-backend/index.js
@@ -30,7 +30,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // --- CORS (LOCAL + RENDER) ---
-const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
+// const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
 
 app.use(
   cors({
